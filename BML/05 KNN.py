10e9ca401594a2962b2d9444c1dcc241fccb7cfa
bunnerman <parnilrbalawade@gmail.com>
2026-09-13 23:03:52 +0530

    Update 05 KNN.py

--- BML/05 KNN.py
+++ BML/05 KNN.py
@@ -24,10 +24,7 @@ knn.fit(X_train_scaled, y_train)
 y_pred = knn.predict(X_test_scaled) # test via predicting
 
 # see model results
-print(f"Accuracy (how close to real value): {accuracy_score(y_test, y_pred):.4f}")
-print(f"Precision (how trustworthy when \"yes\"): {precision_score(y_test, y_pred):.4f}")
-print(f"Recall (how many +ve items correctly found): {recall_score(y_test, y_pred):.4f}")
-print(f"F1-Score (precision + recall): {f1_score(y_test, y_pred):.4f}")
+print(classification_report(y_test, y_pred))
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 #
@@ -35,18 +32,3 @@ print(confusion_matrix(y_test, y_pred))
 #	ACTUAL ⬇️      
 #	CLASS A (+ve)  TRUE POSITIVE     FALSE NEGATIVE
 #	CLASS B (-ve)  FALSE POSITIVE	 TRUE NEGATIVE
-
-# experiment with other values of k
-print(f"{'K Value':<10}{'Accuracy':<10}{'Precision':<10}{'Recall':<10}{'F1-Score':<10}")
-k_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for k in k_values:
-    model = KNeighborsClassifier(n_neighbors=k)
-    model.fit(X_train_scaled, y_train)
-    preds = model.predict(X_test_scaled)
-    
-    acc = accuracy_score(y_test, preds)
-    prec = precision_score(y_test, preds)
-    rec = recall_score(y_test, preds)
-    f1 = f1_score(y_test, preds)
-    
-    print(f"{k:<10}{acc:<10.4f}{prec:<10.4f}{rec:<10.4f}{f1:<10.4f}")
