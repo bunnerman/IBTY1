8d0d4a99dcd7eaf056b414ae0f52b316f447ac78
bunnerman <parnilrbalawade@gmail.com>
2026-09-13 22:47:20 +0530

    Update 05 KNN.py

--- BML/05 KNN.py
+++ BML/05 KNN.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, classification_report
 
 db = load_breast_cancer() # load db
 X = pd.DataFrame(db.data, columns=db.feature_names) # separate X and y
