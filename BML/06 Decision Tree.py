9c66bfa02a0cf103f5e4d09d8aef1dc3a55ae68f
bunnerman <parnilrbalawade@gmail.com>
2026-09-14 14:30:16 +0530

    Update 06 Decision Tree.py

--- BML/06 Decision Tree.py
+++ BML/06 Decision Tree.py
@@ -14,13 +14,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=1, stratify=y
 )
 
-model = DecisionTreeClassifier(criterion='gini', max_depth=3, max_features=2, random_state=1)
+model = DecisionTreeClassifier(criterion='entropy', max_depth=3, max_features=2, random_state=1)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-acc = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {acc:.2f}\n")
 print("Classification Report:\n", classification_report(y_test, y_pred))
 print("Confusion Matrix-\n", confusion_matrix(y_test, y_pred))
 
