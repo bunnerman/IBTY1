b953576c1533f654e5f901c82c435fe4c91a41fb
bunnerman <parnilrbalawade@gmail.com>
2026-09-26 20:54:03 +0530

    Create 11C Lambda Multiple Parameters.java

---
+++ Java/11C Lambda Multiple Parameters.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Maine
+{
+	public static void main(String[] args) 
+	{
+		Test obj = (l, b, h) ->
+		{
+			int vol = l * b * h;
+			return vol;
+		};
+		System.out.print("Enter length, breadth, height: ");;
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println("Volume of Rectangle is " + obj.displayVol(a, b, c));
+	}
+}
+
+interface Test
+{
+	int displayVol(int x, int y, int z);
+}
